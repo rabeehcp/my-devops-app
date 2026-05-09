843ac5da190975a64138ea27bcb85b3ef97b75b2
rabeehcp <rabeehsp3663@gmail.com>
2026-05-09 01:38:04 +0000

    fixed message problem:😂

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({"status": "ok , thenga aaan , neech podoo", "message": "poda kalla thendii , ninakkokke valla kazhivumundodaa kalla kazhuverii!"})
+return jsonify({"status": "ok", "message": "Hello from DevOps!"})
 
 @app.route("/health")
 def health():
     return jsonify({"healthy": True})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)😂
